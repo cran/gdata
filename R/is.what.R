bfe5e80b5fb839ca7ad23a9397df4bc9a875a028
Nitin Jain <nitin.jain@pfizer.com>
2005-10-27 00:00:00 +0000

    version 2.1.1

--- R/is.what.R
+++ R/is.what.R
@@ -2,7 +2,9 @@ is.what <- function(object, verbose=FALSE)
 {
   do.test <- function(test, object)
   {
-    result <- all(get(test)(object))
+    result <- try(get(test)(object), silent=TRUE)
+    if(!is.logical(result) || length(result)!=1)
+      result <- NULL
     return(result)
   }
 
@@ -12,26 +14,19 @@ is.what <- function(object, verbose=FALSE)
   ## Narrow to functions
   is.functions <- is.names[sapply(is.names, function(x) is.function(get(x)))]
 
-  not.using <- c("is.element", "is.empty.model", "is.loaded", "is.mts",
-                 "is.na.data.frame", "is.na.POSIXlt", "is.na<-",
-                 "is.na<-.default", "is.na<-.factor", "is.pairlist", "is.qr",
-                 "is.R", "is.single", "is.unsorted", "is.what")
-  tests <- is.functions[!(is.functions %in% not.using)]
-  names(tests) <- NULL
-  old.warn <- options(warn=-1)
-  results <- sapply(tests, do.test, object=object)
-  options(old.warn)
-  names(results) <- tests
+  tests <- sort(unique(is.functions))
+  results <- suppressWarnings(unlist(sapply(tests, do.test, object=object)))
 
-  if(!verbose)
+  if(verbose)
   {
-    output <- tests[results==TRUE & !is.na(results)]
+    results <- as.character(results)
+    results[results=="TRUE"] <- "T"
+    results[results=="FALSE"] <- "."
+    output <- data.frame(is=results)
   }
   else
   {
-    results[results==TRUE] <- "T"
-    results[results==FALSE] <- "."
-    output <- data.frame(is=I(results))
+    output <- names(results)[results]
   }
 
   return(output)
